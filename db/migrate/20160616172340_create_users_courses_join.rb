9a2fda38e41c9ec2cc9b5797b16d0ce1c9748a61
Vicente Martin <vicentemartintarud@gmail.com>
2016-08-24 14:15:49 -0300

    bug

--- db/migrate/20160616172340_create_users_courses_join.rb
+++ db/migrate/20160616172340_create_users_courses_join.rb
@@ -1,8 +1,8 @@
 class CreateUsersCoursesJoin < ActiveRecord::Migration
   def change
     create_table :users_courses, :id => false do |t|
-      t.integer "courses_id"
       t.integer "user_id"
+      t.integer "courses_id"
     end
     add_index :users_courses, ["user_id", "courses_id"]
   end
