52936d307bbad2aece411694d3b53db78994db49
haroldmuller <hlmuller@uc.cl>
2016-09-12 21:40:59 -0300

    imagen actividad

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -15,6 +15,6 @@
 class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
-  has_attached_file :image, styles: {medium: "600x600>"}
+  has_attached_file :image, styles: {medium: "800x800>"}
   validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf"]
 end
--- app/models/homework.rb
+++ app/models/homework.rb
@@ -13,6 +13,6 @@ class Homework < ActiveRecord::Base
   has_many :key_words
   has_many :answers
   belongs_to :course
-  has_attached_file :image, styles: {medium: "600x600>"}
+  has_attached_file :image, styles: {medium: "800x800>"}
   validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf"]
 end
