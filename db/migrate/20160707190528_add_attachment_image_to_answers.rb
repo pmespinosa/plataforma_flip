d0b94c453a578cd97c3f9765d13fbbfcf2f23777
haroldmuller <hlmuller@uc.cl>
2016-10-06 02:46:40 -0300

    arreglo migracion imagenes

--- db/migrate/20160707190528_add_attachment_image_to_answers.rb
+++ db/migrate/20160707190528_add_attachment_image_to_answers.rb
@@ -12,6 +12,11 @@ class AddAttachmentImageToAnswers < ActiveRecord::Migration
   end
 
   def self.down
-    remove_attachment :answers, :image
+    remove_attachment :answers, :image_responder
+    remove_attachment :answers, :image_argumentar
+    remove_attachment :answers, :image_rehacer
+    remove_attachment :answers, :image_responder_2
+    remove_attachment :answers, :image_argumentar_2
+    remove_attachment :answers, :image_rehacer_2
   end
 end
