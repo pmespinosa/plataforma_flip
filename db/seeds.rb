56d5e9138271242440da32215d700bc0a17d25d1
pmespinosa <pmespinosa@uc.cl>
2016-10-14 00:52:37 -0300

    primer commit antes de neuva app

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,22 +12,16 @@
 User.create(first_name: 'Harold', last_name: 'Muller', email: 'hlmuller@uc.cl', role: 2, password: '123456', asistencia: false)
 User.create(first_name: 'Pablo', last_name: 'Espinosa', email: 'pespinosa@uc.cl', role: 2, password: '123456', asistencia: false)
 User.create(first_name: 'Vicente', last_name: 'Martin', email: 'vrmartin@uc.cl', role: 2, password: '123456', asistencia: false)
-User.create(first_name: 'Vicente', last_name: 'Martin', email: 'vicentemartintarud@gmail.com', role: 0, password: '123456', asistencia: false)
-User.create(first_name: 'Miguel', last_name: 'Nussbaum', email: 'mn@uc.cl', role: 0, password: '123456', asistencia: false)
-User.create(first_name: 'Catalina', last_name: 'Martin', email: 'cata_martin@gmail.com', role: 0, password: '123456', asistencia: false)
-User.create(first_name: 'Camila', last_name: 'Martin', email: 'cami_martin@gmail.com', role: 0, password: '123456', asistencia: false)
-User.create(first_name: 'Patricia', last_name: 'Garcia', email: 'pgh@gmail.com', role: 0, password: '123456', asistencia: false)
 User.create(first_name: 'Guillermo', last_name: 'Ibacache', email: 'gaibacache@uc.cl', role: 0, password: '123456', asistencia: false)
-Course.create(name: "Didáctica de la Lectura", description: "EDU2001-2016/2", users: User.all, course_code: 'EDU2001-2016/1')
 Course.create(name: "Didáctica de las Matemáticas", description: "EDU3004-2016/2", users: User.all, course_code: 'EDU2022-2016/1')
 
 #interpretation = CtHability.create(name: 'Interpretación', description: "To comprehend and express the meaning or significance of a wide
 #variety of experiences, situations, data, events, judgments, conventions, beliefs, rules,
 #procedures or criteria.")
 
-interpretation = CtHability.create(name: 'Interpretación', description: "Complender y expresar el significado de una amplia 
-	variedad de experiencias, situaciones, datos, eventos, juicios de valor, convenciones, creencias, reglas y
-procedimiento de critierios.")
+interpretation = CtHability.create(name: 'Interpretación', description: "To comprehend and express the meaning or significance of a wide
+variety of experiences, situations, data, events, judgments, conventions, beliefs, rules,
+procedures or criteria.")
 
 analysis = CtHability.create(name: 'Análisis', description: "To identify the intended and actual inferential relationships among
 statements, questions, concepts, descriptions or other forms of representation intended to
