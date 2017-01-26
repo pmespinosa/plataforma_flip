24b47c05ead689e3315c31646469d227c9533c86
pmespinosa <pmespinosa@uc.cl>
2017-01-26 16:09:23 -0300

    requerir cmapos evalform

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -21,6 +21,19 @@ class Tree < ActiveRecord::Base
   has_and_belongs_to_many :reports
   has_many :user_tree_performances, :dependent => :destroy
 
+  validates_presence_of :content, :message => "Por favor, ingresa un contenido para evaluación"
+  validates_presence_of :initial_content_question, :message => "Por favor, ingresa la Pregunta inicial de contenido"
+  validates_presence_of :initial_ct_question, :message => "Por favor, ingresa la Pregunta inicial de pensamiento crítico"
+  validates_presence_of :recuperative_content_question, :message => "Por favor, ingresa la Pregunta recuperativa de contenido"
+  validates_presence_of :recuperative_ct_question, :message => "Por favor, ingresa la Pregunta recuperativa de pensamiento crítico"
+  validates_presence_of :deeping_content_question, :message => "Por favor, ingresa la Pregunta de profundización de contenido"
+  validates_presence_of :deeping_ct_question, :message => "Por favor, ingresa la Pregunta de profundización de pensamiento crítico"
+  validates_presence_of :initial_simple_feedback, :message => "Por favor, ingresa el Feedback inicial para Contenido"
+  validates_presence_of :initial_complex_feedback, :message => "Por favor, ingresa el Feedback inicial para Argumento"
+  validates_presence_of :recuperative_simple_feedback, :message => "Por favor, ingresa el Feedback Pregunta Recuperativa para Contenido"
+  validates_presence_of :recuperative_complex_feedback, :message => "Por favor, ingresa el Feedback Pregunta Recuperativa para Argumento"
+  validates_presence_of :deeping_simple_feedback, :message => "Por favor, ingresa el Feedback Pregunta de profundización para Contenido"
+  validates_presence_of :deeping_complex_feedback, :message => "Por favor, ingresa el Feedback Pregunta de profundización para Argumento"
 
   accepts_nested_attributes_for :content, :reject_if => lambda { |a| a[:text].blank? }, :allow_destroy => true
   #accepts_nested_attributes_for :content_questions, :reject_if => lambda { |a| a[:question].blank? }, :allow_destroy => true
